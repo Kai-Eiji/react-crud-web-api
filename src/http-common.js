214805e7e070f79364488934fa5f5976d74d3f89
Kai-Eiji <kaiburkholder.jp@gmail.com>
2020-10-04 08:05:49 -0400

    change api url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://yu3p46x8q5.execute-api.us-east-2.amazonaws.com/production",
   headers: {
     "Content-type": "application/json"
   }
