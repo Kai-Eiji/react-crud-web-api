9261242390ac8bbc871a285286403002430c3041
Kai-Eiji <kaiburkholder.jp@gmail.com>
2020-10-05 04:41:48 -0400

    improve auth

--- src/App.js
+++ src/App.js
@@ -77,49 +77,58 @@ class App extends Component {
 
   render() {
     const { user } = this.state;
-    return (
-      <div>
+    console.log(user);
+    if(user){
+      return (
         <div>
-          <AmplifySignOut />
-          <nav className="navbar navbar-expand navbar-dark bg-dark">
-            <a href="/tutorials" className="navbar-brand">
-              bezKoder
-            </a>
-            <div className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link to={"/tutorials"} className="nav-link">
-                  Tutorials
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to={"/add"} className="nav-link">
-                  Add
-                </Link>
-              </li>
+          <div>
+            <AmplifySignOut />
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+              <a href="/tutorials" className="navbar-brand">
+                bezKoder
+              </a>
+              <div className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <Link to={"/tutorials"} className="nav-link">
+                    Tutorials
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={"/add"} className="nav-link">
+                    Add
+                  </Link>
+                </li>
+              </div>
+            </nav>
+  
+            <div className="container mt-3">
+              <Switch>
+                <Route exact path={["/", "/tutorials", "/callback.com"]} component={TutorialsList} />
+                <Route exact path="/add" component={AddTutorial} />
+                <Route path="/tutorials/:id" component={Tutorial} />
+              </Switch>
             </div>
-          </nav>
-
-          <div className="container mt-3">
-            <Switch>
-              <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
-              <Route exact path="/add" component={AddTutorial} />
-              <Route path="/tutorials/:id" component={Tutorial} />
-            </Switch>
           </div>
+  
+          
         </div>
-
+  
+  
+      );
+    }
+    else{
+      return(
         <div>
-          <p>User: {user ? JSON.stringify(user.attributes) : 'None'}</p>
-          {user ? (
-            <button onClick={() => Auth.signOut()}>Sign Out</button>
-          ) : (
-            <button onClick={() => Auth.federatedSignIn()}>Federated Sign In</button>
-          )}
+            <p>User: {user ? JSON.stringify(user.attributes) : 'None'}</p>
+            {user ? (
+              <button onClick={() => Auth.signOut()}>Sign Out</button>
+            ) : (
+              <button onClick={() => Auth.federatedSignIn()}>Federated Sign In</button>
+            )}
         </div>
-      </div>
-
-
-    );
+      );
+    }
+    
   }
 }
 
